b830d8d5fa414ca96f4fcc8fabf31a9d9022fdb9
Johandre van Deventer <johandrevdeventer@gmail.com>
2025-06-11 14:09:25 +0200

    implement main function to read and display device details from Excel file

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+"""Main script"""
+
+from utils.files import files
+from config import config
+
+
 def main():
-    pass
+    devices_details_path = files.get_devices_details_path(
+        config.DEVICES_DETAILS_FILE_NAME
+    )
+
+    df = files.read_excel_file(devices_details_path, "Details")
+
+    print(df.head())
 
 
 if __name__ == "__main__":
