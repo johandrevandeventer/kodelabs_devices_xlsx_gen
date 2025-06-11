c777d3eb1230f2bd54bb0602fae9ff75a7bd19fd
Johandre van Deventer <johandrevdeventer@gmail.com>
2025-06-11 14:04:40 +0200

    add utility functions for file handling

---
+++ utils/files/files.py
@@ -0,0 +1,48 @@
+"""Utility functions for file handling."""
+
+import pandas as pd
+
+from pathlib import Path
+
+
+def get_devices_details_path(file_name: str) -> Path:
+    """
+    Get the path to the devices details file.
+
+    Args:
+        file_name (str): The name of the file.
+    Returns:
+        Path: The path to the devices details file.
+    """
+    cwd = Path.cwd()
+    return cwd / "details" / file_name
+
+
+def get_devices_xlsx_path(dir_name: str) -> Path:
+    """
+    Get the path to the devices XLSX file.
+
+    Args:
+        dir_name (str): The name of the directory.
+
+    Returns:
+        Path: The path to the devices XLSX file.
+    """
+    cwd = Path.cwd()
+    return cwd / dir_name
+
+
+def read_excel_file(file_path: Path, sheet_name: str) -> pd.DataFrame:
+    """
+    Read an excel file and return a pandas dataframe
+
+    Args:
+        file_path (Path): Path to the excel file
+        sheet_name (str): Name of the sheet to read from
+
+    Returns:
+        pd.DataFrame: Dataframe of the excel sheet
+    """
+
+    df = pd.read_excel(file_path, sheet_name=sheet_name)
+    return df
